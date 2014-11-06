aa1dff2e4408e52aa98f60c33b029a29fa9d2ef8
David Kocsis <david.kocsis@stylersonline.com>
2014-11-06 15:35:04 +0100

    file upload

--- application/views/admin/others/elfinder_mce.php
+++ application/views/admin/others/elfinder_mce.php
@@ -8,7 +8,7 @@
 		<script type="text/javascript" src="<?php echo base_url('res/js/shared/jquery-1.11.1.min.js') ?>"></script>
 		<script type="text/javascript" src="<?php echo base_url('res/js/shared/jquery-ui.min.js') ?>"></script>
 
-		<link rel="stylesheet" type="text/css" href="<?php echo base_url('res/css/admin/smoothness/jquery-ui-1.10.4.custom.min.css') ?>" />
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url('res/css/admin/smoothness/jquery-ui.min.css') ?>" />
 
 		<!-- elFinder CSS (REQUIRED) -->
 		<link rel="stylesheet" type="text/css" href="<?php echo base_url('res/js/admin/tinymce/plugins/elfinder/css/elfinder.min.css') ?>" />
